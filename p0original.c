a447c13c0c478cf37e14f342174c3bec7791a877
Akshath .S <>
2022-07-28 10:00:06 +0000

    my first program

--- p0original.c
+++ p0original.c
@@ -1,1 +1,6 @@
- 
+#include<stdio.h>
+int main(void)
+{
+  printf("My name is Akshath bhandiwad\n");
+ return 0;
+}
--- p1original.c
+++ p1original.c
@@ -1,2 +1,9 @@
+#include<stdio.h>
+int main(void)
+{
+  int input();
+  int add(int 1 , int 2);
+  void output(int1, int2 intsum);
+  return 0;
 
 }
